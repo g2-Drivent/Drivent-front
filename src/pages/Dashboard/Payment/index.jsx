8511a05d0222ff9b2007ae053438fc1fb5daf792
andressafirmino <andressafirmino78@gmail.com>
2023-10-19 14:13:21 -0300

    feat: layout e config sem dados dinâmicos

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -1,3 +1,39 @@
+import styled from 'styled-components';
+import TicketData from '../../../components/Dashboard/Payment/TicketData';
+import CardComponent from '../../../components/Dashboard/Payment/CardContainer';
+import ConfirmComponent from '../../../components/Dashboard/Payment/ConfirmContainer';
+
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+
+  return (
+    <PaymentContainer>
+      <h1>Ingresso e Pagamento</h1>
+      {/* <TicketData/>
+
+      <CardComponent /> */}
+
+      <ConfirmComponent />
+
+    </PaymentContainer>
+
+
+  );
 }
+
+const PaymentContainer = styled.div`
+
+display: flex;
+  flex-direction: column;
+  h1{
+    font-size: 34px;
+    font-weight: 400;
+    font-family: 'Roboto', sans-serif;
+    color: #000000; 
+    margin-left: 5px;
+  }
+`
+
+
+
+
