ff9ab05ea749465d4da7514081818f85f0f3efa4
Pedro Nascimento <sn.jaypee@gmail.com>
2023-10-25 18:17:50 -0300

    Add loading feedback on Activity

--- src/components/Activities/Activity.jsx
+++ src/components/Activities/Activity.jsx
@@ -2,38 +2,54 @@ import styled from "styled-components"
 import enter from "../../assets/enter_icon.svg";
 import check from "../../assets/green_check.svg";
 import cross from "../../assets/red_cross.svg";
+import Loader from "react-loader-spinner";
+import { useState } from "react";
 
 // Status enum:
 // "joined"
 // "open"
 // "closed"
+// "loading" when waiting server response. 
 
 // eslint-disable-next-line react/prop-types
-export function Activity({title, subTitle, duration = 60, spacesLeft, status = "open"}) {
+export function Activity({activityId, title, subTitle, duration = 60, spacesLeft, status = "open"}) {
+
+    const [internalStatus, setInternalStatus] = useState(status);
+
+    function Join() {
+        if(internalStatus !== "open") return;
+
+        setInternalStatus("loading");
+        console.log(activityId);
+        // Call API. (And expect to ActivityBoard refresh the page);
+    }
 
     return (
-        <ActivityComponent $duration={duration} $status={status} >
+        <ActivityComponent $duration={duration} $status={internalStatus} >
             <div>
                 <h1>{title ?? "Sem título"}</h1>
                 <h2>{subTitle ?? "Horário não especificado"}</h2>
             </div>
 
-            <button>
+            <button onClick={Join}>
                 {
                     // Button icon selection based on status argument.
-                    status === "open" && spacesLeft ? <img src={enter}/> : 
-                    status === "joined"             ? <img src={check}/> : 
-                    status === "closed"             ? <img src={cross}/> :
-                    `Invalid activity status ${status}.`
+                    internalStatus === "open" && spacesLeft ? <img src={enter}/> : 
+                    internalStatus === "joined"             ? <img src={check}/> : 
+                    internalStatus === "closed"             ? <img src={cross}/> :
+                    internalStatus === "loading"            ? <Loader color="#FA4098" height={32} width={32} type="ThreeDots"/> :
+                    `Invalid activity status ${internalStatus}.`
                 }
                 {
                     // Button text selection also based on status arg.
-                    status === "open" && spacesLeft ? <p>{spacesLeft} vagas</p> : 
-                    status === "joined"             ? <p>Inscrito</p> : 
-                    status === "closed"             ? <p>Esgotado</p> :
-                    `Invalid activity status ${status}.`
+                    internalStatus === "open" && spacesLeft ? <p>{spacesLeft} vagas</p> : 
+                    internalStatus === "joined"             ? <p>Inscrito</p> : 
+                    internalStatus === "closed"             ? <p>Esgotado</p> :
+                    internalStatus === "loading"            ? "" :
+                    `Invalid activity status ${internalStatus}.`
                 }
             </button>
+
 
         </ActivityComponent>
     )
@@ -60,7 +76,7 @@ const ActivityComponent = styled.div`
         width: 100%;
 
         padding: 12px 0px 12px 10px;
-	background: ${p => p.$status === "joined" ? "#D0FFDB" : "transparent"};
+	    background: ${p => p.$status === "joined" ? "#D0FFDB" : "transparent"};
 
         font-family: "Roboto", sans-serif;
         font-weight: 700;
@@ -79,11 +95,25 @@ const ActivityComponent = styled.div`
         height: 80%;
         width: 100px;
 
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
         border: none;
         border-left: 2px solid ${p => p.$status === "joined" ? "#99E8A1" : "#CFCFCF"};
 	    
-	background: ${p => p.$status === "joined" ? "#D0FFDB" : "transparent"};
+	    background: ${p => p.$status === "joined" ? "#D0FFDB" : "transparent"};
 
         color: ${p => p.$status === "closed" ? "#CC6666" : "#078632"};
+
+        // loader css
+        div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0;
+            margin: 0;
+        }
     }
 `
