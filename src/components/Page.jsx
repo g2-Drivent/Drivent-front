d29b15f7775acdd5169d35d40e7ce4747dd5e670
Renata Inacio <revazgauska@gmail.com>
2023-10-27 19:58:45 -0300

    fix background image

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  background: ${props => props.$background};
+  background-image: url(${props => props.$background});
   background-size: cover;
   min-height: 100vh;
   width: 100%;
